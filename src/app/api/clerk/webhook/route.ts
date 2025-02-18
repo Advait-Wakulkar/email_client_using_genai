1d53a9709893ff46d6e69fe798746281b5f36443
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-02-17 22:48:22 -0500

    add Clerk webhook API route for user creation and update middleware for public route matching

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,31 @@
+// /api/clerk/webhook
+
+import { db } from "@/server/db"
+
+export const POST = async (req : Request)=>{
+    const {data} = await req.json()
+    console.log("clerk webhook received", data)
+    const emailAddress = data.email_addresses[0].email_address
+    const firstName = data.first_name
+    const lastName = data.last_name
+    const imageUrl = data.image_url
+    const id = data.id
+
+    const db_req = await db.user.create({
+        data : {
+            id : id,
+            emailAddress : emailAddress,
+            firstName : firstName,
+            lastName : lastName,
+            imageUrl : imageUrl
+        }
+    })
+
+    if (db_req){
+        return new Response("User Created", {status : 200})
+    }else{
+        return new Response ("Unable to create User")
+    }
+
+    
+}
